cba4078d361623877bac834b5c145926bc6ebd16
bretwang <bretwang@outlook.com>
2017-11-16 18:11:30 +0900

    log4j2 config init

--- src/main/java/ctrl/AccountController.java
+++ src/main/java/ctrl/AccountController.java
@@ -2,6 +2,8 @@ package ctrl;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,18 @@ public class AccountController {
 	@RequestMapping("/account")
 	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
 		System.out.println("AABB");
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+		
+//		String log4jPath = AccountController.class.getClassLoader().getResource("").getPath()+"log4j.properties";
+//		System.out.println(log4jPath);
+//		PropertyConfigurator.configure("D:/p-wang/2017/workspace/eclipse/ProjectX1-DataCenter/src/main/resources/log4j.properties");
+		
+		Logger logger  =  LoggerFactory.getLogger(AccountController.class);
+		logger.debug("Let's celcebrate12345678--debug");
+		logger.info("Let's celcebrate12345678--info");
+		logger.warn("Let's celcebrate12345678--warn");
+		logger.error("Let's celcebrate12345678--error");
+		
+        //Application.console(AccountController.class, "123456798");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
--- src/main/java/ctrl/Application.java
+++ src/main/java/ctrl/Application.java
@@ -1,12 +1,18 @@
 package ctrl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-    public static void main(String[] args) {
+	
+	public static void console(Class<?> cls, String info) {
+		Logger logger  =  LoggerFactory.getLogger(cls);
+		logger.debug("Let's celcebrate !www");
+	}
+    public static void main(String[] args) {        
         SpringApplication.run(Application.class, args);
     }
 }
